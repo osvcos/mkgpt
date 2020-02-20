b84e6a2d8025d9fcfb9a10c6b69bc8a64d21107f
osvcos <osvaldogcosta90@gmail.com>
2020-02-20 18:01:30 -0300

    Implement parameter parsing

--- mkgpt.c
+++ mkgpt.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 void usage()
 {
@@ -15,6 +16,41 @@ int main(int argc, char *argv[])
         printf("Too many arguments.\n");
         usage();
         return -1;
+    }
+    else if(argc == 1)
+    {
+        printf("Missing parameters.\n");
+        usage();
+        return -1;
+    }
+    else
+    {
+        for(int i = 1; i < argc; i++)
+        {
+            if(argv[i][0] == '-')
+            {
+                if(strcmp("--force", argv[i]) == 0)
+                {
+                    if(argc == 2)
+                    {
+                        printf("You must specify a device.\n");
+                        usage();
+                        return -1;
+                    }
+                    force = 1;
+                }
+                else
+                {
+                    printf("Unknown parameter %s\n", argv[i]);
+                    usage();
+                    return -1;
+                }
+            }
+            else
+            {
+                devname = argv[i];
+            }
+        }
     }
     return 0;
 }
