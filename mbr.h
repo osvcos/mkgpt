bd1c7cc1b166943e866942ec38e7cf1d4d7741cb
osvcos <osvaldogcosta90@gmail.com>
2020-02-21 08:03:54 -0300

    Add a structure for MBR partition record

--- mbr.h
+++ mbr.h
@@ -1,11 +1,21 @@
 #ifndef MBR_H
 #define MBR_H
 
+typedef struct {
+    unsigned char boot_indicator;
+    unsigned char starting_chs[3];
+    unsigned char os_type;
+    unsigned char ending_chs[3];
+    unsigned int starting_lba;
+    unsigned int size_in_lba;
+    
+} __attribute__((packed)) partition_record;
+
 struct master_boot_record {
     char boot_code[440];
     unsigned int disk_signature;
     unsigned short unknown;
-    unsigned long long parttitions[8];
+    partition_record parttitions[4];
     unsigned short signature;
 } __attribute__((packed));
 
