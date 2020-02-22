17b822ab25f044a3f087af494b1daaae724981ef
osvcos <osvaldogcosta90@gmail.com>
2020-02-22 15:45:11 -0300

    Add GPT header and improve initialization function

--- gpt.c
+++ gpt.c
@@ -1,7 +1,32 @@
+#include <string.h>
+#include <unistd.h>
+
 #include "device.h"
+#include "mbr.h"
 #include "gpt.h"
 
 int initialize_gpt(struct device *dev)
 {
+    gpt_header gpt;
+
+    memset(&gpt, 0, sizeof(gpt_header));
+    if(create_mbr(dev, MBR_TYPE_PROTECTIVE) == -1)
+        return -1;
+    if(seek_lba(1, dev) == -1)
+        return -1;
+
+    memcpy(gpt.signature, "EFI PART", sizeof(gpt.signature));
+
+    gpt.revision[0] = 0x00;
+    gpt.revision[1] = 0x00;
+    gpt.revision[2] = 0x01;
+    gpt.revision[3] = 0x00;
+
+    gpt.header_size = sizeof(gpt_header);
+
+    gpt.my_lba = 1;
+    gpt.alternate_lba = (dev->size / dev->lsz);
+    if((write(dev->descriptor, &gpt, sizeof(gpt_header)) == -1))
+        return -1;
     return 0;
 }
--- gpt.h
+++ gpt.h
@@ -3,6 +3,23 @@
 
 #include "device.h"
 
+typedef struct {
+    unsigned char signature[8];
+    unsigned char revision[4];
+    unsigned int header_size;
+    unsigned int header_crc32;
+    unsigned int reserved;
+    unsigned long long my_lba;
+    unsigned long long alternate_lba;
+    unsigned long long first_usable_lba;
+    unsigned long long last_usable_lba;
+    unsigned short disk_guid[8];
+    unsigned long long partition_entry_lba;
+    unsigned int number_of_partition_entries;
+    unsigned int size_of_partition_entry;
+    unsigned int partition_entry_array_crc32;
+} __attribute__((packed)) gpt_header;
+
 int initialize_gpt(struct device *dev);
 
 #endif // GPT_H
