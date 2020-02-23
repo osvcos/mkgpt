561f76b2cd7938adc17a3ec6708f3bb38aeedbd8
osvcos <osvaldogcosta90@gmail.com>
2020-02-23 09:33:42 -0300

    Fix CRC32 GPT Header calculation

--- gpt.c
+++ gpt.c
@@ -24,11 +24,15 @@ int initialize_gpt(struct device *dev)
     gpt.revision[3] = 0x00;
 
     gpt.header_size = sizeof(gpt_header);
-    gpt.header_crc32 = crc32(0L, (const Bytef*) &gpt, sizeof(gpt_header));
 
     gpt.my_lba = 1;
     gpt.alternate_lba = (dev->size / dev->lsz);
     gpt.first_usable_lba = ((16385 / dev->lsz) + 2);
+
+    gpt.header_crc32 = crc32(0L, Z_NULL, 0);
+    for(int i = 0; i < sizeof(gpt_header); i++)
+        gpt.header_crc32 = crc32(gpt.header_crc32, (unsigned char*) &gpt + i, 1);
+
     if((write(dev->descriptor, &gpt, sizeof(gpt_header)) == -1))
         return -1;
     return 0;
