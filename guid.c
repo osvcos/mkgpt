fa2cf3a2a3aefa3571f7f394e4144dd37d219717
osvcos <osvaldogcosta90@gmail.com>
2020-03-12 17:38:26 -0300

    guid: simplify gen_v4_efi_guid

--- guid.c
+++ guid.c
@@ -7,10 +7,6 @@
 
 void gen_v4_efi_guid(guid_t *guid)
 {
-    unsigned short node1;
-    unsigned short node2;
-    unsigned short node3;
-
     srand(time(NULL));
     memset(guid, 0, sizeof(guid_t));
     guid->time_low = rand() % RAND_MAX;
@@ -21,10 +17,10 @@ void gen_v4_efi_guid(guid_t *guid)
     guid->clock_seq_hi_and_reserved = rand() % RAND_MAX;
     guid->clock_seq_hi_and_reserved &= 0x3F;
     guid->clock_seq_hi_and_reserved |= 0x80;
-    node1 = rand() % RAND_MAX;
-    guid->node1 = swap_u16(node1);
-    node2 = rand() % RAND_MAX;
-    guid->node2 = swap_u16(node2);
-    node3 = rand() % RAND_MAX;
-    guid->node3 = swap_u16(node3);
+    guid->node1 = rand() % RAND_MAX;
+    guid->node1 = swap_u16(guid->node1);
+    guid->node2 = rand() % RAND_MAX;
+    guid->node2 = swap_u16(guid->node2);
+    guid->node3 = rand() % RAND_MAX;
+    guid->node3 = swap_u16(guid->node3);
 }
